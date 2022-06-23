af152bc5a6cfdd9da568ca1fffe6f93b989b0037
Dilshod <dilshod.sherzodovich.2004@gmail.com>
2022-06-23 13:14:00 +0500

    forum style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,38 +1,47 @@
 import React from "react";
-import "./Home.scss"
+import "./home.scss";
+import { Link } from "react";
 
 function Home() {
   return (
     <>
       <div className="about-unibook-section">
         <div className="container">
-          <h1>About <a href="#">Unibook.uz</a></h1>
+          <h1>
+            About <Link href="#">Unibook.uz</Link>
+          </h1>
           <div className="row">
             <div className="col-lg-6">
-              <img src="https://picsum.photos/500/300?random=1" alt="random picture" />
+              <img
+                src="https://picsum.photos/500/300?random=1"
+                alt="randomPicture"
+              />
             </div>
             <div className="col-lg-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-              molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-              numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-              optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-              obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-              nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-              quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-              sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-              recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
-              minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
-              quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
-              fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
-              consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-              doloremque. Quaerat provident commodi consectetur veniam similique ad
-              earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
-              fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
-              suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-              modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
-              totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
-              quasi aliquam eligendi, placeat qui corporis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+              fugiat iusto fuga praesentium optio, eaque rerum! Provident
+              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+              eius earum ut molestias architecto voluptate aliquam nihil,
+              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+              tenetur error, harum nesciunt ipsum debitis quas aliquid.
+              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
+              laudantium molestias eos sapiente officiis modi at sunt excepturi
+              expedita sint? Sed quibusdam recusandae alias error harum maxime
+              adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+              Officiis iure rerum voluptates a cumque velit quibusdam sed amet
+              tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat,
+              temporibus enim commodi iusto libero magni deleniti quod quam
+              consequuntur! Commodi minima excepturi repudiandae velit hic
+              maxime doloremque. Quaerat provident commodi consectetur veniam
+              similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+              pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+              excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+              Voluptatem quaerat non architecto ab laudantium modi minima sunt
+              esse temporibus sint culpa, recusandae aliquam numquam totam
+              ratione voluptas quod exercitationem fuga. Possimus quis earum
+              veniam quasi aliquam eligendi, placeat qui corporis!
             </div>
           </div>
         </div>
@@ -45,7 +54,10 @@ function Home() {
               <div className="card">
                 <div className="card-header">Temiov Ulug'bek</div>
                 <div className="card-body">
-                  <img src="https://picsum.photos/500/300?random=2" alt="team-member" />
+                  <img
+                    src="https://picsum.photos/500/300?random=2"
+                    alt="team-member"
+                  />
                 </div>
               </div>
             </div>
